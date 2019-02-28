85413f520b8e11d36a2ac75811417a632b7c24d8
David Bolin <david@bolin.at>
2019-02-28 09:09:29 -0800

    finished refactoring in Namespacing

--- scripts/store.js
+++ scripts/store.js
@@ -1,19 +1,61 @@
 'use strict';
-/* global cuid */
+/* global cuid, Item */
 
 const store = (function () {
   const items = [
-    { id: cuid(), name: 'apples', checked: false },
-    { id: cuid(), name: 'oranges', checked: false },
-    { id: cuid(), name: 'milk', checked: true },
-    { id: cuid(), name: 'bread', checked: false }
   ];
   const hideCheckedItems = false;
   const searchTerm = '';
 
+  function findById(id){
+    return store.items.find(ele => id === ele.id);
+  }
+
+  function addItem(name){
+    try {
+      Item.validateName(name);
+      const item = Item.create(name);
+      this.items.push(item);
+    } catch(error) {
+      console.log(`Cannot add item: ${error.message}`);
+    }
+  }
+
+  function findAndToggleChecked(id) {
+    this.findById(id).checked = !this.findById(id).checked;
+  }
+
+  function findAndUpdateName(id, newName) {
+    try {
+      Item.validateName(newName);
+      this.findById(id).name = newName;
+    } catch(error) {
+      console.log(`Cannot update name: ${error.message}`);
+    }
+  }
+
+  function findAndDelete(id) {
+    this.items = this.items.filter(ele => ele.id !== id);
+  }
+
+  function toggleCheckedFilter(){
+    this.hideCheckedItems = !this.hideCheckedItems;
+  }
+
+  function setSearchTerm(term) {
+    this.searchTerm = term;
+  }
+
   return {
     items,
     hideCheckedItems,
-    searchTerm
-  }
+    searchTerm,
+    findById,
+    addItem,
+    findAndToggleChecked,
+    findAndUpdateName,
+    findAndDelete,
+    toggleCheckedFilter,
+    setSearchTerm,
+  };
 }());
